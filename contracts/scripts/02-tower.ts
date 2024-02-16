36c080a55f58f65ed9a1a3e85b783b1485f187a8
FrankBevr <frank_dierolf@web.de>
2024-02-16 14:04:24 +0100

    :zap: (contracts) add 01-tower.ts to scripts

---
+++ contracts/scripts/02-tower.ts
@@ -0,0 +1,23 @@
+import { getDeploymentData } from '@/utils/getDeploymentData'
+import { initPolkadotJs } from '@/utils/initPolkadotJs'
+import { writeContractAddresses } from '@/utils/writeContractAddresses'
+import { deployContract } from '@scio-labs/use-inkathon/helpers'
+
+const main = async () => {
+  const initParams = await initPolkadotJs()
+  const { api, chain, account } = initParams
+
+  const { abi, wasm } = await getDeploymentData('tower')
+  const tower = await deployContract(api, account, abi, wasm, 'default', [])
+
+  await writeContractAddresses(chain.network, {
+    tower,
+  })
+}
+
+main()
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
+  .finally(() => process.exit(0))
