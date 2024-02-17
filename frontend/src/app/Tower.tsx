547d1f791c0d7b4ce7643eaffcc51ffb7a1a2646
FrankBevr <frank_dierolf@web.de>
2024-02-17 17:47:44 +0100

    :bug: (frontend) fix preload

--- frontend/src/app/Tower.tsx
+++ frontend/src/app/Tower.tsx
@@ -1,8 +1,10 @@
 import * as THREE from 'three';
-useGLTF.preload("./hamburger.glb");
+useGLTF.preload("./towerSquare_sampleA.glb");
+
 import { Clone, useGLTF } from "@react-three/drei";
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
 const floorMaterial = new THREE.MeshStandardMaterial({ color: "limegreen" });
+
 export function Tower() {
   const model = useGLTF("./towerSquare_sampleA.glb");
   return (
@@ -15,7 +17,7 @@ export function Tower() {
         material={floorMaterial}
       >
       </mesh>
-      <Clone object={model.scene} scale={1} />
+      <Clone object={model.scene} scale={0.5} />
     </>
   );
 }
