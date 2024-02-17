672671b81441f703af5f27e900c53417bb4e2b39
FrankBevr <frank_dierolf@web.de>
2024-02-17 09:12:47 +0100

    :zap: (frontend) clean up page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,16 +2,31 @@
 
 import { useEffect } from 'react'
 
-import { useInkathon } from '@scio-labs/use-inkathon'
+import { contractQuery, decodeOutput, useInkathon, useRegisteredContract } from '@scio-labs/use-inkathon'
 import { toast } from 'react-hot-toast'
 
 import { HomePageTitle } from '@/app/components/home-page-title'
 import { ChainInfo } from '@/components/web3/chain-info'
 import { ConnectButton } from '@/components/web3/connect-button'
-import { GreeterContractInteractions } from '@/components/web3/greeter-contract-interactions'
+import { ContractIds } from '@/deployments/deployments'
 
 export default function HomePage() {
-  // Display `useInkathon` error messages (optional)
+  const { api } = useInkathon();
+  const { contract: contractGreeter } = useRegisteredContract(
+    ContractIds.Greeter,
+  );
+
+  const getGreeter = async () => {
+    if (!contractGreeter || !api) return;
+    const result = await contractQuery(api, "", contractGreeter, "greet");
+    const { output, isError, decodedOutput } = decodeOutput(
+      result,
+      contractGreeter,
+      "greet",
+    );
+    console.log(output)
+    if (isError) throw new Error(decodedOutput);
+  }
   const { error } = useInkathon()
   useEffect(() => {
     if (!error) return
@@ -20,21 +35,8 @@ export default function HomePage() {
 
   return (
     <>
-      <div className="container relative flex grow flex-col items-center justify-center py-10">
-        {/* Title */}
-        <HomePageTitle />
-
-        {/* Connect Wallet Button */}
-        <ConnectButton />
-
-        <div className="mt-12 flex w-full flex-wrap items-start justify-center gap-4">
-          {/* Chain Metadata Information */}
-          <ChainInfo />
-
-          {/* Greeter Read/Write Contract Interactions */}
-          <GreeterContractInteractions />
-        </div>
-      </div>
+      <button onClick={getGreeter}>Get Greeter</button>
+      <ConnectButton />
     </>
   )
 }
