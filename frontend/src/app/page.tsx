6ef0c76b93bdc52b7f04229d8d3b815c84d7f8ef
FrankBevr <frank_dierolf@web.de>
2024-02-17 09:19:07 +0100

    :art: (frontend) pnpm run lint:format

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,30 +2,29 @@
 
 import { useEffect } from 'react'
 
-import { contractQuery, decodeOutput, useInkathon, useRegisteredContract } from '@scio-labs/use-inkathon'
+import { ContractIds } from '@/deployments/deployments'
+import {
+  contractQuery,
+  decodeOutput,
+  useInkathon,
+  useRegisteredContract,
+} from '@scio-labs/use-inkathon'
 import { toast } from 'react-hot-toast'
 
 import { HomePageTitle } from '@/app/components/home-page-title'
 import { ChainInfo } from '@/components/web3/chain-info'
 import { ConnectButton } from '@/components/web3/connect-button'
-import { ContractIds } from '@/deployments/deployments'
 
 export default function HomePage() {
-  const { api } = useInkathon();
-  const { contract: contractGreeter } = useRegisteredContract(
-    ContractIds.Greeter,
-  );
+  const { api } = useInkathon()
+  const { contract: contractGreeter } = useRegisteredContract(ContractIds.Greeter)
 
   const getGreeter = async () => {
-    if (!contractGreeter || !api) return;
-    const result = await contractQuery(api, "", contractGreeter, "greet");
-    const { output, isError, decodedOutput } = decodeOutput(
-      result,
-      contractGreeter,
-      "greet",
-    );
+    if (!contractGreeter || !api) return
+    const result = await contractQuery(api, '', contractGreeter, 'greet')
+    const { output, isError, decodedOutput } = decodeOutput(result, contractGreeter, 'greet')
     console.log(output)
-    if (isError) throw new Error(decodedOutput);
+    if (isError) throw new Error(decodedOutput)
   }
   const { error } = useInkathon()
   useEffect(() => {
--- frontend/src/deployments/deployments.ts
+++ frontend/src/deployments/deployments.ts
@@ -1,8 +1,8 @@
+import { address as addressGreeter } from '@inkathon/contracts/deployments/greeter/development'
+import abiGreeter from '@inkathon/contracts/deployments/greeter/greeter.json'
 import { SubstrateDeployment } from '@scio-labs/use-inkathon'
 
 import { env } from '@/config/environment'
-import abiGreeter from "@inkathon/contracts/deployments/greeter/greeter.json";
-import { address as addressGreeter } from "@inkathon/contracts/deployments/greeter/development";
 
 /**
  * Add or change your custom contract ids here
@@ -16,11 +16,11 @@ export const getDeployments = async (): Promise<SubstrateDeployment[]> => {
   const deployments: SubstrateDeployment[] = []
 
   deployments.push({
-    contractId: "greeter",
-    networkId: "development",
+    contractId: 'greeter',
+    networkId: 'development',
     abi: abiGreeter,
     address: addressGreeter,
-  });
+  })
 
   return deployments
 }
